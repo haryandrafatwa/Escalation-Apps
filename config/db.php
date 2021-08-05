d115a87a46c4beac56b77aba75673ac8dd5c84fa
haryandrafatwa <haryandrafatwa010@gmail.com>
2021-08-05 08:26:33 +0700

    Last Commit

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=localhost;dbname=escalationApps;port=5432',
     'username' => 'postgres',
-    'password' => 'password',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
